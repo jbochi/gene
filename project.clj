df3a851c198c74fe9baeb3f7f41c9a4b081d5c6d
Juarez Bochi <jbochi@gmail.com>
2013-11-30 14:52:16 -0200

    Add url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject gene "0.1.0-SNAPSHOT"
   :description "Parallel Genetic Algorithm in Clojure"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/jbochi/gene"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]

72d43255db28f7c7472c51fe2e5cdfedf38ae144
Juarez Bochi <jbochi@gmail.com>
2013-11-30 14:50:41 -0200

    Split evolution logic from problem

--- src/gene/core.clj
+++ src/gene/core.clj
@@ -1,6 +1,23 @@
-(ns gene.core)
+(ns gene.core
+    (:use gene.data gene.uncap gene.evolve))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def n 15)
+(def p 10)
+
+(defn score [warehouses]
+  (- (cost cost-matrix warehouses)))
+
+(def max-generations 20)
+
+(def population-size 20)
+
+(def initial-population
+  (take population-size
+        (repeatedly #(random-set n p))))
+
+(def final-generation (evolve score initial-population max-generations #(mutate % n) crossover))
+
+(def solution (first (select-n score final-generation 1)))
+
+(println solution)
+(println (score solution))
--- src/gene/evolve.clj
+++ src/gene/evolve.clj
@@ -1,46 +1,24 @@
-(ns gene.evolve
-  (:use gene.data gene.uncap))
+(ns gene.evolve)
 
-(def n 15)
-(def p 10)
-
-(defn score [warehouses]
-  (- (cost cost-matrix warehouses)))
-
-(defn breed [parents]
+(defn breed [crossover parents]
   (apply crossover
     (partition 2 (shuffle parents))))
 
 (defn select-n [score population n]
   (take n (reverse (sort-by score population))))
 
-(defn next-generation [score population]
+(defn next-generation [score population mutate crossover]
   (let [most-fit (select-n score population (/ (count population) 4))]
     (concat
       most-fit
-      (map #(mutate % n) most-fit)
-      (mapcat (fn [_] breed most-fit) (range 2))
+      (map mutate most-fit)
+      (mapcat (fn [_] (partial crossover breed) most-fit) (range 2))
      )))
 
 (def evolve
-  (fn [score population max-generations]
+  (fn [score population max-generations mutate crossover]
     (loop [gen population
            cnt max-generations]
       (if (zero? cnt)
         gen
-        (recur (next-generation score gen) (dec cnt))))))
-
-;;;;;;;;;;;;;;;;;
-
-(def max-generations 20)
-
-(def population-size 20)
-
-(def initial-population
-  (take population-size
-        (repeatedly #(random-set n p))))
-
-(def solution (first (select-n score (evolve score initial-population max-generations) 1)))
-
-(println solution)
-(println (score solution))
+        (recur (next-generation score gen mutate crossover) (dec cnt))))))

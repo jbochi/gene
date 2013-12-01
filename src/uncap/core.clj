0228c1f27ae9ca02868a28fd57fd718c1c57a5d3
Juarez Bochi <jbochi@gmail.com>
2013-12-01 18:24:59 -0200

    Make solver accept input filename

--- src/uncap/core.clj
+++ src/uncap/core.clj
@@ -3,15 +3,16 @@
           [uncap.reader :only [read-file]]
           uncap.dna))
 
-(let [data (read-file "test/uncap/data/cap71.txt")
-      {:keys [cost-matrix warehouse-costs m]} data
-      problem {:score #(- (cost cost-matrix warehouse-costs %))
-               :random-solution #(random-set m (inc (rand-int m)))
-               :mutate #(mutate % m)
-               :crossover crossover
-               :population-size 20
-               :n-generations 20}
-    final-generation (evolve problem)
-    solution (first final-generation)]
-        (println solution)
-        (println (cost cost-matrix warehouse-costs solution)))
+(defn solve [filename]
+  (let [data (read-file filename)
+        {:keys [cost-matrix warehouse-costs m]} data
+        problem {:score #(- (cost cost-matrix warehouse-costs %))
+                 :random-solution #(random-set m (inc (rand-int m)))
+                 :mutate #(mutate % m)
+                 :crossover crossover
+                 :population-size 20
+                 :n-generations 20}
+      final-generation (evolve problem)
+      solution (first final-generation)]
+        {:solution solution
+         :cost (cost cost-matrix warehouse-costs solution)}))

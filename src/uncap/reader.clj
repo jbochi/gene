4e021a4e7e29face9a0a8940566662a74fcb23ab
Juarez Bochi <jbochi@gmail.com>
2013-12-01 17:27:14 -0200

    Refactor parsing

--- src/uncap/reader.clj
+++ src/uncap/reader.clj
@@ -8,8 +8,8 @@
   (let [text (slurp filename)
         numbers (parse-numbers text)
         [m n] (take 2 numbers)
-        warehouse-costs (take m (take-nth 2 (drop 3 numbers)))
-        customer-numbers (drop (+ (* 2 m) 2) numbers)
+        warehouse-costs (->> numbers (drop 3) (take-nth 2) (take m))
+        customer-numbers (->> numbers (drop 2) (drop (* m 2)))
         demands (take-nth (inc m) customer-numbers)]
     {:n n
      :m m

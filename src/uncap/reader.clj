b071dd0e4e1914a7ed49ba25a5b6a88a5e606091
Juarez Bochi <jbochi@gmail.com>
2013-12-01 16:21:49 -0200

    Refactor line parsing

--- src/uncap/reader.clj
+++ src/uncap/reader.clj
@@ -2,8 +2,11 @@
   (:require [clojure.java.io :as io])
   (:use [clojure.string :only [split trim]]))
 
+(defn- read-numbers [rdr]
+  (map read-string (split (trim (.readLine rdr)) #"\s")))
+
 (defn read-file [filename]
   (let [rdr (io/reader filename)
-        first-line (.readLine rdr)
-        [m n] (map read-string (split (trim first-line) #"\s"))]
-    {:n n :m m}))
+        [m n] (read-numbers rdr)]
+    {:n n
+     :m m}))
